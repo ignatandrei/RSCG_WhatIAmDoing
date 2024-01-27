d3e0defc369d123d50d2e25bdb6028cb5f50e086
Andrei Ignat <ignatandrei@yahoo.com>
2024-01-27 14:38:29 +0200

    data

--- src/WIAD/WIAD_DemoConsole/Program.cs
+++ src/WIAD/WIAD_DemoConsole/Program.cs
@@ -1,18 +1,18 @@
 ﻿using WIAD_DemoConsole;
 using static System.Console;
 
-Console.WriteLine("Heasdasdallo, World!");
-string nameFile = "test.txt";
-if (File.Exists(nameFile))
-{
-    File.Delete(nameFile);
-}
+//Console.WriteLine("Heasdasdallo, World!");
+//string nameFile = "test.txt";
+//if (File.Exists(nameFile))
+//{
+//    File.Delete(nameFile);
+//}
 
-File.WriteAllText(nameFile, "Hello, World!");
+//File.WriteAllText(nameFile, "Hello, World!");
 //File.WriteAllText(nameFile, "Hello, World!", Encoding.ASCII);
 
 //Console.WriteLine(WIAD_DemoConsole.FibMemory.Fibonaci(7));
-//Console.WriteLine(FibNoMemory.Fibonaci(7));
+Console.WriteLine(FibNoMemory.Fibonaci(7));
 
 WriteLine("static");
 
