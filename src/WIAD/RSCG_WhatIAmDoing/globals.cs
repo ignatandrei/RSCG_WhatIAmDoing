7dffab266a6a6c29ee026c7872be4f1830e260f5
Andrei Ignat <ignatandrei@yahoo.com>
2024-01-31 19:54:03 +0200

    no scriban

--- src/WIAD/RSCG_WhatIAmDoing/globals.cs
+++ src/WIAD/RSCG_WhatIAmDoing/globals.cs
@@ -2,7 +2,7 @@
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using Microsoft.CodeAnalysis.Operations;
 global using Microsoft.CodeAnalysis.Text;
-global using Scriban;
+//global using Scriban;
 global using System.Text.RegularExpressions;
 global using System.Xml.Linq;
 global using System.Diagnostics;
